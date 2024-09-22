3f12288573c1fa6455a72d01d12cbd5ace1aae80
Jonathan Lalande <lalandej.gg@gmail.com>
2024-09-21 20:40:48 -0400

    Profile commits endpoint
    Wrap the `commits` endpoint of the API in a cProfile context, and dump
    the stats.
    
    This dump can then be used with a visualization tool like `snakeviz` to
    see what takes the most CPU in that API endpoint.

--- Backend/api/routers/commits_router.py
+++ Backend/api/routers/commits_router.py
@@ -1,3 +1,6 @@
+import cProfile
+import datetime
+
 from fastapi import APIRouter, HTTPException
 from typing import List
 from api.schemas.common_schema import CommitDetail
@@ -9,4 +12,10 @@ pydriller_service = PydrillerService()
 
 @commits_router.get("/", response_model=List[CommitDetail])
 def fetch_all_commits(repo_path: str, days: int = 30):
-    return pydriller_service.fetch_all_commits(repo_path, days)
+    with cProfile.Profile() as pr:
+        commits = pydriller_service.fetch_all_commits(repo_path, days)
+
+        now = int(datetime.datetime.now().timestamp())
+        pr.dump_stats("%s_commits_route.prof" % (now))
+
+        return commits
